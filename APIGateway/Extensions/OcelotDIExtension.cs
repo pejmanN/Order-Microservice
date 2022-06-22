f4527e4e08308ca7a7e07169e3d83ccdffc38d16
p.nazraz <pejman.nazraz@gmail.com>
2022-06-22 01:09:53 -0700

    Add Ocelot ApiGateway

---
+++ APIGateway/Extensions/OcelotDIExtension.cs
@@ -0,0 +1,14 @@
+﻿using Ocelot.DependencyInjection;
+
+namespace APIGateway.Extensions
+{
+    public static class OcelotDIExtension
+    {
+        public static void AddOcelot(this WebApplicationBuilder builder)
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            builder.Configuration.AddJsonFile($"ocelot.{env}.json", optional: false, reloadOnChange: true);
+            builder.Services.AddOcelot();
+        }
+    }
+}
---
+++ APIGateway/Program.cs
@@ -0,0 +1,22 @@
+using APIGateway.Extensions;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddOcelot();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+await app.UseOcelot();
+
+app.Run();
